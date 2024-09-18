38d740383e93b3d654bfda5bb5ac0436da722de8
Fin <vladislav.2002.vn@gmail.com>
2024-09-18 22:21:31 +0300

    💚 Fix building.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     build: {
-        lib: { entry: resolve(__dirname, 'src/index.ts'), formats: ['es'] },
+        lib: {
+            entry: resolve(__dirname, 'src/index.ts'),
+            formats: ['es'],
+            fileName: 'index',
+        },
+        minify: 'esbuild',
     },
     resolve: { alias: { '@': resolve('src/') } },
+    plugins: [
+        dts({
+            insertTypesEntry: true,
+        }),
+    ],
 });
